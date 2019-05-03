db7b356e2a21e7785b76bd5bbc1fe223b52a1d2f
Michael Caveney <dylanesque27@gmail.com>
2019-05-03 11:50:21 -0400

    Refined separation of concerns with buttons and app functioning

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const buttonData = [
-  { id: 'one', char: '1'},
-  { id: 'two', char: '2'},
-  { id: 'three', char: '3'},
-  { id: 'add', char: '+'},
-  { id: 'four', char: '4'},
-  { id: 'five', char: '5'},
-  { id: 'six', char: '6'},
-  { id: 'subtract', char: '-'},
-  { id: 'seven', char: '7'},
-  { id: 'eight', char: '8'},
-  { id: 'nine', char: '9'},
-  { id: 'multiply', char: '*'},
-  { id: 'zero', char: '0'},
-  { id: 'decimal', char: '.'},
-  { id: 'divide', char: '/'},
+// Concatenate a space on either side of these symbols when added to the display formula
+const operatorButtons = [
+  { id: "add", char: "+" },
+  { id: "subtract", char: "-" },
+  { id: "multiply", char: "*" },
+  { id: "divide", char: "/" }
 ];
+
+const numberButtons = [
+  { id: "one", char: "1" },
+  { id: "two", char: "2" },
+  { id: "three", char: "3" },
+  { id: "four", char: "4" },
+  { id: "five", char: "5" },
+  { id: "six", char: "6" },
+  { id: "seven", char: "7" },
+  { id: "eight", char: "8" },
+  { id: "nine", char: "9" },
+  { id: "zero", char: "0" },
+];
+
+
 
 function App() {
 
-
+  const [display, setDisplay] = useState("0");
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Calculator App</h1>
         <main>
-        <h3 id="display">Edit me later</h3>
+          <h3 id="display">{display}</h3>
 
-        <div className="buttons">
-        {buttonData.map(button => (
-          <button
-            id={button.id}
-            onClick={this.handleClick}>{button.char}</button>
-        ))}
-        <button id="clear">C</button>
-
-
-        </div>
+          <div className="buttons">
+            {numberButtons.map(button => (
+              <button id={button.id}>
+                {button.char}
+              </button>
+            ))}
+            {operatorButtons.map(button => (
+              <button id={button.id}>
+                {button.char}
+              </button>
+            ))}
+            <button id="clear">C</button>
+            <button id="decimal">.</button>
+            <button id="equals">=</button>
+          </div>
         </main>
-
       </header>
-
     </div>
   );
 }
@@ -51,12 +59,6 @@ export default App;
 
 /*
 
-My calculator should contain a clickable element containing an = (equal sign) with a corresponding id="equals".
-User Story #2: My calculator should contain 10 clickable elements containing one number each from 0-9, with the following corresponding IDs: id="zero", id="one", id="two", id="three", id="four", id="five", id="six", id="seven", id="eight", and id="nine".
-User Story #3: My calculator should contain 4 clickable elements each containing one of the 4 primary mathematical operators with the following corresponding IDs: id="add", id="subtract", id="multiply", id="divide".
-User Story #4: My calculator should contain a clickable element containing a . (decimal point) symbol with a corresponding id="decimal".
-User Story #5: My calculator should contain a clickable element with an id="clear".
-User Story #6: My calculator should contain an element to display values with a corresponding id="display".
 User Story #7: At any time, pressing the clear button clears the input and output values, and returns the calculator to its initialized state; 0 should be shown in the element with the id of display.
 User Story #8: As I input numbers, I should be able to see my input in the element with the id of display.
 User Story #9: In any order, I should be able to add, subtract, multiply and divide a chain of numbers of any length, and when I hit =, the correct result should be shown in the element with the id of display.
